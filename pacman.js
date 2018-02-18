6d09805494aaca4a8508d2465ec92c6ec9ad17a4
bencusb <bencusbalogh01@gmail.com>
2018-02-18 18:07:11 +0100

    added dots

--- pacman.js
+++ pacman.js
@@ -2,29 +2,29 @@ playground =
 {
     "map":
 	[
-		"----------------------------",
-		"-____________--____________-",		
-		"-_----_-----_--_-----_----_-",
-		"-_----_-----_--_-----_----_-",
-		"-__________________________-",
-		"-_----_--_--------_--_----_-",
-		"-______--____--____--______-",
-		"------_-----_--_-----_------",
-		"------_-----_--_-----_------",
-		"------_--__________--_------",
-		"------_--_--------_--_------",
-		"-_________--------_________-",
-		"------_--_--------_--_------",
-		"------_--__________--_------",
-		"------_--_--------_--_------",
-		"-____________--____________-",
-		"-_----_-----_--_-----_----_-",
-		"-___--________________--___-",
-		"---_--_--_--------_--_--_---",
-		"-______--____--____--______-",
-		"-_----------_--_----------_-",
-		"-__________________________-",
-		"----------------------------"
+		"XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+		"X............XX............X",
+		"X.XXXX.XXXXX.XX.XXXXX.XXXX.X",
+		"X.XXXX.XXXXX.XX.XXXXX.XXXX.X",
+		"X..........................X",
+		"X.XXXX.XX.XXXXXXXX.XX.XXXX.X",
+		"X......XX....XX....XX......X",
+		"XXXXXX.XXXXX.XX.XXXXX.XXXXXX",
+		"XXXXXX.XXXXX.XX.XXXXX.XXXXXX",
+		"XXXXXX.XX..........XX.XXXXXX",
+		"XXXXXX.XX.XXXXXXXX.XX.XXXXXX",
+		"X     ....XXXXXXXX....     X",
+		"XXXXXX.XX.XXXXXXXX.XX.XXXXXX",
+		"XXXXXX.XX..........XX.XXXXXX",
+		"XXXXXX.XX.XXXXXXXX.XX.XXXXXX",
+		"X............XX............X",
+		"X.XXXX.XXXXX.XX.XXXXX.XXXX.X",
+		"X...XX................XX...X",
+		"XXX.XX.XX.XXXXXXXX.XX.XX.XXX",
+		"X......XX....XX....XX......X",
+		"X.XXXXXXXXXX.XX.XXXXXXXXXX.X",
+		"X..........................X",
+		"XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     ]
 };
 
@@ -36,7 +36,7 @@ function onLoad()
 function onDraw()
 {
 	var ctx = document.getElementById('canvas').getContext('2d')
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 	
 	cell_width = Math.round(ctx.canvas.width / playground.map[0].length);
 	cell_height = Math.round(ctx.canvas.height / playground.map.length);
@@ -44,9 +44,15 @@ function onDraw()
 	{
 		for(var l = 0; l < playground.map[i].length; l++)
 		{
-			if(playground.map[i][l] == '-')
+			if(playground.map[i][l] == 'X')	// wall
 			{
+				ctx.fillStyle = "#1111ff"
 				ctx.fillRect(l*cell_width, i*cell_height, cell_width, cell_height);
+			}
+			else if(playground.map[i][l] == '.') // dot
+			{
+				ctx.fillStyle = "#111111"
+				ctx.fillRect(l*cell_width + 2*cell_width/5 , i*cell_height + 2*cell_height/5, (cell_width)/5, (cell_height)/5);
 			}
 		}
 	}
