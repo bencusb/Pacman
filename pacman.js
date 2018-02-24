23277b22e6b86158bad01f38346f4f3fdacff4a0
bencusb <bencusbalogh01@gmail.com>
2018-02-24 16:38:15 +0100

    basic packman movement added

--- pacman.js
+++ pacman.js
@@ -26,28 +26,35 @@ gamestate =
 		"X..........................X",
 		"XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     ],
-	"pacman": {"x": 1, "y": 1}
+	"keys": {"W_pressed": false, "S_pressed":false,"A_pressed":false,"D_pressed":false},
+	"pacman": {"x": 13, "y": 17, "dir": "up"}
 };
 
 function onLoad()
 {
-	window.setInterval(onTick, 20);
+	window.setInterval(onTick, 15);
 	document.onkeydown = onKeyDown;
+	document.onkeyup = onKeyUp;
 }
 
 function onKeyDown(event) 
+{	
+	var key_press = String.fromCharCode(event.keyCode);
+	var key_code = event.keyCode;	
+	if(key_press == "W") gamestate.keys.W_pressed=true;
+	if(key_press == "S") gamestate.keys.S_pressed=true;
+	if(key_press == "A") gamestate.keys.A_pressed=true;
+	if(key_press == "D") gamestate.keys.D_pressed=true;
+}
+
+function onKeyUp(event)
 {
 	var key_press = String.fromCharCode(event.keyCode);
 	var key_code = event.keyCode;
-	if(key_press == "W"){
-		console.log("w");
-	} else if(key_press == "S") {
-		console.log("s");
-	} else if(key_press == "A") {
-		console.log("a");
-	} else if(key_press == "D") {
-		console.log("d");
-	}
+	if(key_press == "W") gamestate.keys.W_pressed=false;
+	if(key_press == "S") gamestate.keys.S_pressed=false;
+	if(key_press == "A") gamestate.keys.A_pressed=false;
+	if(key_press == "D") gamestate.keys.D_pressed=false;
 }
 
 function onTick()
@@ -55,11 +62,35 @@ function onTick()
 	updatePacman();
 	updateGhosts();
 	draw();
+	
+	//console.log( gamestate.keys );
 }
 
 function updatePacman()
 {
-	gamestate.pacman.x += 0.05;
+	var speed = 0.1;
+	
+	if( gamestate.keys.W_pressed )
+		gamestate.pacman.dir = "up";
+	if( gamestate.keys.S_pressed )
+		gamestate.pacman.dir = "down";
+	if( gamestate.keys.A_pressed )
+		gamestate.pacman.dir = "left";
+	if( gamestate.keys.D_pressed )
+		gamestate.pacman.dir = "right";
+	
+	if( gamestate.pacman.dir == "up" )
+		gamestate.pacman.y -= speed;
+	if( gamestate.pacman.dir == "down" )
+		gamestate.pacman.y += speed;
+	if( gamestate.pacman.dir == "left" )
+		gamestate.pacman.x -= speed;
+	if( gamestate.pacman.dir == "right" )
+		gamestate.pacman.x += speed;
+	
+	//row = Math.round(gamestate.pacman.y)
+	//col = Math.round(gamestate.pacman.x)
+	//gamestate.map[row] = gamestate.map[row].substr(0,col) + ' ' + gamestate.map[row].substr(col+1);
 }
 
 function updateGhosts()
